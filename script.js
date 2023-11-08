ec698876ed2594bf5180248713ec2dd2c103593f
dhannah <dominic.hannah@DETNSW>
2023-11-09 10:50:00 +1100

    1

--- script.js
+++ script.js
@@ -63,12 +63,14 @@ window.addEventListener('load', function () {
       this.height = this.spriteHeight;
       this.spriteX = this.collisionX - this.width * 0.5;
       this.spriteY = this.collisionY - this.height * 0.5 - 70;
+      this.frameX = Math.floor(Math.random() * 4);
+      this.frameY = Math.floor(Math.random() * 3);
     }
     draw(context) {
       context.drawImage(
         this.image,
-        0,
-        0,
+        this.frameX * this.spriteWidth,
+        0 * this.spriteHeight,
         this.spriteWidth,
         this.spriteHeight,
         this.spriteX,
@@ -91,6 +93,7 @@ window.addEventListener('load', function () {
       this.canvas = canvas;
       this.width = this.canvas.width;
       this.height = this.canvas.height;
+      this.topMargin = 260;
       this.player = new Player(this);
       this.numberOfObstacles = 10;
       this.obstacles = [];
@@ -135,13 +138,15 @@ window.addEventListener('load', function () {
           const dx = testObstacle.collisionX - obstacle.collisionX;
           const dy = testObstacle.collisionY - obstacle.collisionY;
           const distance = Math.hypot(dy, dx);
+          const distanceBuffer = 150;
           const sumOfRadii =
-            testObstacle.collisionRadius + obstacle.collisionRadius;
+            testObstacle.collisionRadius + obstacle.collisionRadius + distanceBuffer;
           if (distance < sumOfRadii) {
             overlap = true;
           }
         });
-        if (!overlap) {
+        const margin = testObstacle.collisionRadius * 2;
+        if (!overlap && testObstacle.spriteX > 0 && testObstacle.spriteX < this.width - testObstacle.width && testObstacle.collisionY > this.topMargin + margin && testObstacle.collisionY < this.height - margin) {
           this.obstacles.push(testObstacle);
         }
         attempts++;
